4e03783b6d61a1f3cc1afc1338bc254b7f669300
Cerrebe <cerrebe3000@gmail.com>
2023-10-20 02:23:07 +0200

    hecho más bonito el index

--- pages/index.js
+++ pages/index.js
@@ -19,16 +19,17 @@ export default function Home({ bans, part }) {
 			</Head>
 			<div className='absolute w-full h-full bg-slate-800 -z-30 m-0'>
 				<div className='z-30'>
-					<div className='block mx-80 my-10 p-10'>
+					<div className='my-10 py-10 text-center mx-4 px-4'>
 						<Link
 							href='/abyss'
-							className='bg-cyan-700 float-left ml-48 p-10 rounded-xl'
+							className='bg-cyan-700 p-10 rounded-xl align-middle inline-block border-8 mx-8 my-2'
 						>
 							Abismo
 						</Link>
+						<div className='lg:px-16 rounded-xl align-middle inline-block lg:mx-8'></div>
 						<Link
 							href='/fighterz'
-							className='bg-cyan-700 float-right mr-48 p-10 rounded-xl'
+							className='bg-cyan-700 p-10 rounded-xl align-middle inline-block border-8 mx-8 my-2'
 						>
 							Fighterz
 						</Link>
